0cf81cfa0e73d2b8e6dab83826d61ac9dbc6f42c
Jorge Medina <medidrones@gmail.com>
2025-09-12 22:04:38 -0300

    Criando a primeira função Lambda

---
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -0,0 +1,29 @@
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class ProductsAppStack extends cdk.Stack {
+  readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        runtime: lambda.Runtime.NODEJS_20_X,
+        memorySize: 512,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+      }
+    );
+  }
+}
